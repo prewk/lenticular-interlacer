e6a03d6f4137385ffb1d099c82d07a336d166050
Oskar Thornblad <oskar.thornblad@gmail.com>
2019-01-18 15:41:07 +0100

    Hello

---
+++ src/__tests__/interlacer.test.ts
@@ -0,0 +1,91 @@
+import Interlacer from '../interlacer'
+
+const pixels = (times: number, rgb: number[]): number[] => {
+  const out: number[] = [];
+  for (let i = 0; i < times; i++) {
+    out.push(...rgb);
+  }
+  return out;
+}
+
+describe('Interlacer', () => {
+    it('should vertically interlace 2 images', () => {
+        const interlacer = new Interlacer(1, 2, false);
+
+        // Add a 5x5 image with 4 channels
+        interlacer.push(
+            Buffer.from([
+                ...pixels(5, [255, 0, 0, 255]),
+                ...pixels(5, [128, 0, 0, 255]),
+                ...pixels(5, [64, 0, 0, 0]),
+                ...pixels(5, [32, 0, 0, 0]),
+                ...pixels(5, [16, 0, 0, 0]),
+            ]),
+            4,
+            5,
+            5
+        );
+
+        // Add a 4x4 image with 3 channels
+        interlacer.push(
+            Buffer.from([
+                ...pixels(4, [0, 255, 0]),
+                ...pixels(4, [0, 128, 0]),
+                ...pixels(4, [0, 64, 0]),
+                ...pixels(4, [0, 32, 0]),
+            ]),
+            3,
+            4,
+            4
+        );
+
+        const results = interlacer.interlace();
+
+        expect(Array.from(results.buffer)).toEqual([
+            ...pixels(2, [255, 0, 0]), ...pixels(2, [0, 255, 0]),
+            ...pixels(2, [128, 0, 0]), ...pixels(2, [0, 128, 0]),
+            ...pixels(2, [64, 0, 0]), ...pixels(2, [0, 64, 0]),
+            ...pixels(2, [32, 0, 0]), ...pixels(2, [0, 32, 0]),
+        ]);
+    });
+
+  it('should horizontally interlace 2 images', () => {
+    const interlacer = new Interlacer(1, 2, true);
+
+    // Add a 5x5 image with 4 channels
+    interlacer.push(
+        Buffer.from([
+            ...pixels(5, [255, 0, 0, 255]),
+            ...pixels(5, [128, 0, 0, 255]),
+            ...pixels(5, [64, 0, 0, 0]),
+            ...pixels(5, [32, 0, 0, 0]),
+            ...pixels(5, [16, 0, 0, 0]),
+        ]),
+        4,
+        5,
+        5
+    );
+
+    // Add a 4x4 image with 3 channels
+    interlacer.push(
+        Buffer.from([
+            ...pixels(4, [0, 255, 0]),
+            ...pixels(4, [0, 128, 0]),
+            ...pixels(4, [0, 64, 0]),
+            ...pixels(4, [0, 32, 0]),
+        ]),
+        3,
+        4,
+        4
+    );
+
+    const results = interlacer.interlace();
+
+    expect(Array.from(results.buffer)).toEqual([
+        ...pixels(4, [255, 0, 0]),
+        ...pixels(4, [128, 0, 0]),
+        ...pixels(4, [0, 64, 0]),
+        ...pixels(4, [0, 32, 0]),
+    ]);
+  });
+});
---
+++ src/interlacer.ts
@@ -0,0 +1,128 @@
+interface Image {
+    buffer: Buffer,
+    channels: number,
+    width: number,
+    height: number,
+}
+
+/**
+ * Interlaces raw pixel buffers from multiple images into one suitable for lenticular printing purposes
+ */
+export default class Interlacer {
+    images: Image[] = [];
+
+    constructor(
+        private lpi: number,
+        private dpi: number,
+        private horizontal: boolean
+    ) {}
+
+    /**
+     * Add another image to be interlaced
+     */
+    public push(buffer: Buffer, channels: number, width: number, height: number): this {
+        this.images.push({
+            buffer,
+            channels,
+            width,
+            height,
+        });
+
+        return this;
+    }
+
+    /**
+     * Find the minimum dimensions possible
+     */
+    private getMVDimensions(): { width: number, height: number } {
+        return this.images.reduce((mvd, image) => {
+            if (image.width < mvd.width) {
+                mvd.width = image.width;
+            }
+            if (image.height < mvd.height) {
+                mvd.height = image.height;
+            }
+
+            return mvd;
+        }, { width: Infinity, height: Infinity });
+    }
+
+    /**
+     * Extract one RGB pixel from a raw image buffer
+     */
+    private getRgbPixel(image: Image, x: number, y: number, fallback: [number, number, number]): [number, number, number] {
+        if (x >= image.width || y >= image.height) return fallback;
+
+        const offset = y * (image.width * image.channels);
+
+        return typeof image.buffer[offset + x * image.channels + 2]  !== 'undefined'
+            ? [
+                image.buffer[offset + x * image.channels + 0],
+                image.buffer[offset + x * image.channels + 1],
+                image.buffer[offset + x * image.channels + 2],
+            ]
+            : fallback;
+    }
+
+    /**
+     * Interlace the images into one
+     */
+    public interlace(): Image {
+        const images = this.images;
+
+        if (!images.length) {
+            throw new Error('No images to interlace!');
+        }
+
+        let pointer = 0;
+
+        /**
+         * Cycle through the image buffers
+         */
+        const cycle = () => {
+            pointer++;
+            if (!images[pointer]) {
+                pointer = 0;
+            }
+        };
+
+        const { width, height } = this.getMVDimensions();
+        const output: number[] = [];
+        const lensThickness = this.dpi / this.lpi;
+
+        if (this.horizontal) {
+            // Horizontal interlace
+            for (let y = 0; y < height; y++) {
+                if (y && y % lensThickness === 0) {
+                    cycle();
+                }
+
+                const image = images[pointer];
+                for (let x = 0; x < width; x++) {
+                    output.push(...this.getRgbPixel(image, x, y, [255, 255, 255]));
+                }
+            }
+        } else {
+            // Vertical interlace
+            for (let y = 0; y < height; y++) {
+                pointer = 0;
+
+                for (let x = 0; x < width; x++) {
+                    if (x && x % lensThickness === 0) {
+                        cycle();
+                    }
+
+                    const image = images[pointer];
+                    output.push(...this.getRgbPixel(image, x, y, [255, 255, 255]));
+                }
+            }
+        }
+
+        return {
+            buffer: Buffer.from(output),
+            channels: 3,
+            width,
+            height,
+        };
+    }
+}
